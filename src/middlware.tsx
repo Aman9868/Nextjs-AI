758110010f0658777319cf551dae2db90785f039
Aman9868 <9868638501aman@gmail.com>
2025-09-20 16:42:44 +0530

    Fix middleware

--- src/middlware.tsx
+++ src/middlware.tsx
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export function middleware(req) {
+export function middleware(req: NextRequest) {
   const token = req.cookies.get("authToken")?.value;
 
   if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
